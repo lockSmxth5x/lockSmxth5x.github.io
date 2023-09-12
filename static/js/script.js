2534e72e20c31db62231a1e6cf85fb0e07454143
lockSmxth5x <115736993+lockSmxth5x@users.noreply.github.com>
2023-09-11 21:13:00 -0400

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function home_link() {
+  document.location.href = "./";
+}
+function git_profile_link() {
+  document.location.href = "https://github.com/LockSmxth5x";
+}
+function repl_profile_link() {
+  document.location.href = "https://replit.com/@lockSmxth5x";
+}
+function game_site_link() {
+  document.location.href = "https://locksmxth5x.github.io/home";
+}
+function contact_link() {
+  document.location.href = "#";
+}
+
+feather.replace();
